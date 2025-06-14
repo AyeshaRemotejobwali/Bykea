6909422262630e671e3d3a6ed9cabbcacdedc2e9
AyeshaRemotejobwali <ayeshasajidts@gmail.com>
2025-06-14 09:13:26 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$username = "uxgukysg8xcbd";
+$password = "6imcip8yfmic";
+$dbname = "dbgxcdqtni4xej";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
